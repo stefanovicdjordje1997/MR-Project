23eefc6a73dd59db506db2252ddabd16b14fe40a
Djordje Stefanovic <stefanovic.djordje1997@gmail.com>
2023-05-08 17:48:44 +0200

    Added validators with messages to log-in form

--- src/app/auth/start/log-in/log-in.page.ts
+++ src/app/auth/start/log-in/log-in.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../auth.service";
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-log-in',
@@ -8,10 +9,15 @@ import {Router} from "@angular/router";
   styleUrls: ['./log-in.page.scss'],
 })
 export class LogInPage implements OnInit {
+  logInForm: FormGroup
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.logInForm = new FormGroup({
+      email: new FormControl('Unesite emal',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required, Validators.minLength(8)])
+    })
   }
 
   onLogin() {
